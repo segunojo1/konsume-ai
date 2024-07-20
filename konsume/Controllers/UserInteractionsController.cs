364222cdb3ad7755621b28194e01b77a901a9df6
hasbiyallah01 <oyebohm@gmail.com>
2024-07-21 04:41:33 +1300

    initial

---
+++ konsume/Controllers/UserInteractionsController.cs
@@ -0,0 +1,33 @@
+﻿using KONSUME.Core.Application.Interfaces.Services;
+using KONSUME.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KONSUME.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInteractionsController : ControllerBase
+    {
+        private readonly IUserInteractionService _service;
+
+        public UserInteractionsController(IUserInteractionService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveUserInteraction([FromBody] UserInteractionDto dto)
+        {
+            var interaction = await _service.SaveUserInteractionAsync(dto.Question, dto.Response);
+            return Ok(interaction);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserInteractions()
+        {
+            var interactions = await _service.GetUserInteractionsAsync();
+            return Ok(interactions);
+        }
+    }
+}
+
---
+++ konsume/Core/Domain/Entities/UserInteraction.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KONSUME.Core.Domain.Entities
+{
+    public class UserInteraction :Auditables
+        {
+            public string Question { get; set; }
+            public string Response { get; set; }
+        }
+
+}
