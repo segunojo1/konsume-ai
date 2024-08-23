e359b40c4aa0deb02c8abf68ef18ae57987dcafb
hasbiyallah01 <oyebohm@gmail.com>
2024-08-23 12:38:33 +0100

    implemented chatbot and meal recommendation

--- backend/obj/Release/net8.0/DaticianProj.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/DaticianProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DaticianProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c806bc33ab943e47cfa28ec115070d636527227a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248d72e12f7f5f0fa6bf209f4798b11dc1654cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DaticianProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DaticianProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
