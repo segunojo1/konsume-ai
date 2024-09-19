8447cb5f91992978ad3ab8f5cfe3e19b59edb51f
segunojo1 <olusegunkd2017@gmail.com>
2024-09-19 20:54:01 +0100

    add: konsume icon

--- frontend/konsume/pages/auth/restaurant/index.tsx
+++ frontend/konsume/pages/auth/restaurant/index.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Restaurant = () => {
   return (
-    <div>Restaurant</div>
+    <div className='py-10 px-4 mx-auto max-w-[358px] font-satoshi'>
+      <div>
+        <h3 className='text-desktop-content font-bold mb-8'>
+          Sign up for yourself and get personalized meal recommendations, progress tracking, and more.
+        </h3>
+      </div>
+      <SignupForm />
+    </div>
   )
 }
 
